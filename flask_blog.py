bc85879c9a796951c8d1c9b146b49a9af68d7872
MarcoLiuDDD <101748352+MarcoLiuDDD@users.noreply.github.com>
2022-03-18 14:47:27 -0400

    Update flask_blog.py

--- flask_blog.py
+++ flask_blog.py
@@ -1,1 +1,30 @@
 # use flask to create a blog
+
+# config.py
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+  #Database configuration
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  
+# models.py
+from app import db
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  password = db.Column(db.String(80), nullable=False)
+  email = db/Column(db.String(120), nullable=False)
+  
+  def __repr__(self):
+
+# app.py
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import DQLAlchemy
+from config import Config 
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
